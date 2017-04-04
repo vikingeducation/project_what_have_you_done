12409a7fe3ca40fe35577116415186cfaf0e4b1f
Bill Hefty <billhefty@gmail.com>
2017-04-04 06:43:52 -0500

    test api logic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const sunlightAPI = require('../utils/api')
+const Legistlator = require('../models/legislators')
+
+/* GET legislator by zip */
+router.get('/:zip?', function(req, res, next) {
+    if (!req.query.zip) {
+      next()
+    } else {
+      const zip = req.query.zip
+
+      let legislatorList = []
+
+      sunlightAPI.getLegistlatorByZip(zip)
+          .then((results) => res.send(results))
+          .catch((err) => {
+              console.error(err)
+              res.send(err)
+          })  
+    }      
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
