2d5b3bc7698f043fe51efd66d3cc2ab4c1b9982c
Bill Hefty <billhefty@gmail.com>
2017-04-04 06:42:59 -0500

    add data structure

---
+++ models/legislators.js
@@ -0,0 +1,11 @@
+class Legistlator {
+  constructor(img, name, chamber, party, votes) {
+    this.img = img
+    this.name = name
+    this.chamber = chamber
+    this.party = party
+    this.votes = votes
+  }
+}
+
+module.exports = Legistlator
