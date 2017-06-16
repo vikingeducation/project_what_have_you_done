d8cd1cca17d4397aec1f71a5a613644cfafb5a0d
Luke E Summerscales <ke2uke@virginmedia.com>
2017-06-16 19:15:45 +0100

    listings page displaying legislators

--- js/check.js
+++ js/check.js
@@ -6,12 +6,15 @@ function checkReq() {
   for(var i = 0; i < input.length; i++){
     if(input[i].value == "" || isNaN(input[i].value)) {
       input[i].setAttribute("class", "numput req");
+      input[i].value = "";
     } else {
       input[i].setAttribute("class", "numput");
     }
   }
 }
 checkReq();
+
+console.log("SCRIPT RUNNING");
 for(var i = 0; i < input.length; i++){
   input[i].addEventListener("keyup", function(){
     checkReq();
--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 request = require("request");
+http = require("http");
+fs = require("fs");
+qs = require("querystring");
+Promise = require("promise");
 
-zip = "";
-legislators = [];
+var zip = "";
+var legislators = [];
+var house = "<p>";
 
-var options = {
-  url: "https://congress.api.sunlightfoundation.com/legislators/locate?zip=11216",
-  headers: {
-    "user-agent": "request"
-  }
-};
 function parse(data) {
   var data = JSON.parse(data).results;
   data.forEach(function(item){
@@ -19,13 +18,106 @@ function parse(data) {
     ]
     legislators.push(temp);
   });
+  for(var i = 0; i < legislators.length; i++) {
+    if(legislators[i][2] == "house") {
+      house += legislators[i][0] + " ";
+      house += legislators[i][1] + "<br />";
+    }
+  }
+  house += "</p>";
+  console.log("done");
+  return house;
 }
+
 function byZip(error, response, body){
   if(!error && response.statusCode == 200) {
-    parse(body);
+    var result = parse(body);
+    return result;
   } else {
     console.log(error);
   }
 }
 
-request(options, byZip);
+var server = http.createServer(function(req, response){
+  console.log("Incoming request...");
+  console.log(`Requesting ${req.url}`);
+  switch(req.url){
+    case "/css/style.css":
+      fs.readFile("css/style.css", "utf8", function(err, data){
+        response.writeHead(200, {"Content-Type":"text/css"});
+        response.write(data);
+        response.end();
+        console.log("Success")
+      });
+      break;
+    case "/css/style.css.map":
+      fs.readFile("css/style.css.map", "utf8", function(err, data){
+        response.writeHead(200, {"Content-Type":"text/css"});
+        response.write(data);
+        response.end();
+        console.log("Success")
+      });
+      break;
+    case "/js/check.js":
+      fs.readFile("js/check.js", "utf8", function(err, data){
+        response.writeHead(200, {"Content-Type" : "application/javascript"});
+        response.write(data);
+        response.end();
+        console.log("Success");
+      });
+      break;
+    case "/":
+      fs.readFile("index.html", "utf8", function(err, data){
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        response.write(data);
+        console.log("Success");
+        response.end();
+      });
+      break;
+    case "/listings":
+      zip = "";
+      if(req.method == "POST"){
+        req.on("data", function(data){
+          zip += data;
+          zip = qs.parse(zip);
+          zip = zip["a"] + zip["b"] + zip["c"] + zip["d"] + zip["e"];
+          console.log("Gathering data for ZIP code", zip);
+          options["url"] = `https://congress.api.sunlightfoundation.com/legislators/locate?zip=${zip}`;
+          var promise = new Promise(function(res, rej){
+            request(options, function(err, resu, body){
+              res(byZip(err, resu, body));
+            });
+            console.log("Reqeust Promise")
+          });
+          promise.then(function(msg){
+            console.log("Then Promise")
+            fs.readFile("listings.html", "utf8", function(err, data){
+              response.writeHead(200, {"Content-Type" : "text/html"});
+              data = data.replace(/{{ZIP}}/, zip);
+              data = data.replace(/{{CONTENT}}/, msg)
+              response.write(data);
+              console.log("Success");
+              response.end();
+            });
+          });
+        });
+      }
+      break;
+  }
+  console.log("Responding");
+});
+
+var options = {
+  url: "",
+  headers: {
+    "user-agent": "req"
+  }
+};
+var url = {
+  host: "localhost",
+  port: "3000",
+}
+
+server.listen(url, function(){
+  console.log(`Server listening at ${url["host"]}:${url["port"]}`);
+});
