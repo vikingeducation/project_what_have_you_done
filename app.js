c4a9862e681b6753c9c75b25152cfd05039298b9
Alex-Willenbrink <awillenbrink@gmail.com>
2017-06-28 18:42:18 -0500

    made mods to api queries so they are useable

--- app.js
+++ app.js
@@ -3,40 +3,37 @@
 const request = require("request");
 const _       = require("lodash");
 
-/*
-function zipcodeLegislatorRequest(zipcode){
-  let apiRequestLoc = "https://congress.api.sunlightfoundation.com/legislators/locate?zip=" + zipcode.toString();
-  console.log(apiRequestLoc);
-  request(apiRequestLoc, function(error, response, body) {
+// Returns legislator info related to specified zipcode
+function legislatorInfoByZipCode(zipcode) {
+  return new Promise(function(resolve, reject) {
+    let apiRequestLoc = "https://congress.api.sunlightfoundation.com/legislators/locate?zip=" + zipcode.toString();
+    request(apiRequestLoc, function(error, response, body) {
+      // Print error if occured
+      if(error){
+        reject(error);
+      }
 
-    // Print error if occured
-    console.log("error: ", error);
+      // Print response status code if recieved
+      console.log("statusCode: ", response && response.statusCode);
 
-    // Print response status code if recieved
-    console.log("statusCode: ", response && response.statusCode);
+      // Make an array to store zipcode legislators
+      let legislatorArray = [];
 
-    // Make an array to store zipcode legislators
-    let legislatorArray = [];
+      // Store body information into legislatorArray (organized)
+      bodyStats(JSON.parse(body), legislatorArray);
 
-    // Print html for body (JSON response)
-    bodyStats(JSON.parse(body), legislatorArray);
-
-    // sort the legislatorArray into house and senate for later use
-    let chamberSortedArray = sortLegislators(legislatorArray);
-    let houseArray = chamberSortedArray[0];
-    let senateArray = chamberSortedArray[1];
-
-    printLegislatorInfo(houseArray);
-    printLegislatorInfo(senateArray);
-
+      // sort the legislatorArray into house and senate for later use
+      let chamberSortedArray = sortLegislators(legislatorArray);
+      resolve(chamberSortedArray);
+      //let houseArray = chamberSortedArray[0];
+      //let senateArray = chamberSortedArray[1];
+    });
   });
-}
+};
 
 
 
-
-
-// function for find out info with body
+// function for find out info with body (legislator info)
 function bodyStats(body, legislatorArray){
   _.each(body.results, function(value, key){
     let legislator = new Legislator(
@@ -44,7 +41,7 @@ function bodyStats(body, legislatorArray){
       value.last_name,
       value.party,
       value.chamber,
-      value.bioguide_id
+      value.bioguide_id,
       value.phone,
       value.website
     );
@@ -76,7 +73,6 @@ function printLegislatorInfo(legArray){
 }
 
 
-
 // Sorts legislator info into house and senate
 function sortLegislators(legislatorArray){
   // int arrays
@@ -92,70 +88,75 @@ function sortLegislators(legislatorArray){
   return [legArrayHouse, legArraySenate];
 }
 
-//zipcodeLegislatorRequest(65201);
-//
-//
-//
-//
-//
-//
-//
-*/
 
-/*
-name = vicky hartzler
-zipcode = 65201
-bioguide_id = H001053
-party = R
-HorS = House
- */
+function billInfoRequest(bioguide_id) {
+  return new Promise(function(resolve, reject) {
+    // This will get bill_id and vote (yea or nay) for recent bills voted on
+    let apiRequestBillPartial = "https://congress.api.sunlightfoundation.com/votes?fields=voters." +
+    bioguide_id + ".vote" + ",bill_id";
+
+    request(apiRequestBillPartial, function(error, response, bodyBillsPartial) {
+      if(error){
+        reject(error);
+      }
+
+      // Print response status code if recieved
+      console.log("statusCode: ", response && response.statusCode);
+
+      // Make 2 arrays:
+      // 1st for initial partial bill info (bill_id and vote) => recBillsArrayPartial
+      // 2nd for partial bill info, official_title, and url (urls.govtrack) => recBillsArrayComplete
+
+      let recBillsArrayPartial = [];
+
+      // Print html for body (JSON response)
+      billStatsPartial(JSON.parse(bodyBillsPartial), recBillsArrayPartial, bioguide_id);
+
+      resolve(recBillsArrayPartial);
+    });
+  });
+}
 
 
- function billInfoRequest(bioguide_id){
+// function for find out info with body
+function billStatsPartial(bodyBillsPartial, recBillsArrayPartial, bioguide_id) {
+  // set BillInfo to the meat of our api (contains bill_info and vote)
+  let billInfo = bodyBillsPartial.results;
 
-   // This will get bill_id and vote (yea or nay) for recent bills voted on
-   let apiRequestBillPartial = "https://congress.api.sunlightfoundation.com/votes?fields=voters." +
-   bioguide_id + ".vote" + ",bill_id";
+  // Add all bills and corresponding votes to recBillsArrayPartial
+  for(let i = 0; i < billInfo.length; i++) {
+    if(billInfo[i].bill_id !== undefined){
+      let billPartial = new BillPartial(
+        billInfo[i].bill_id,
+        billInfo[i].voters[bioguide_id].vote
+      );
+      // add the new billPartial to our array
+      recBillsArrayPartial.push(billPartial);
+    }
+  }
+}
 
-   request(apiRequestBillPartial, function(error, response, bodyBillsPartial) {
-     // Print error if occured
-     console.log("error: ", error);
+// Constructor for billPartial (bill_id and vote in regars to bill_id)
+function BillPartial(bill_id, vote) {
+  this.bill_id = bill_id;
+  this.vote = vote;
+}
 
-     // Print response status code if recieved
-     console.log("statusCode: ", response && response.statusCode);
+// Extract rest of info from legislator bills
+function getCompleteBill(billPartial) {
+  return new Promise((resolve, reject) => {
+    let apiRequestBillRest = "https://congress.api.sunlightfoundation.com/bills?bill_id=" + billPartial.bill_id;
+    request(apiRequestBillRest, function(error, response, bodyBillsRest) {
+      if(error) {
+        reject(error);
+      }
 
-     /*
-     Make 2 arrays:
-     1st for initial partial bill info (bill_id and vote) => recBillsArrayPartial
-     2nd for partial bill info, official_title, and url (urls.govtrack) => recBillsArrayComplete
-     */
-     let recBillsArrayPartial = [];
-     let recBillsArrayComplete = [];
-
-     // Print html for body (JSON response)
-     billStatsPartial(JSON.parse(bodyBillsPartial), recBillsArrayPartial, bioguide_id);
-     
-
-     // Extract rest of information for the bills of legislator
-     _.each(recBillsArrayPartial, function(value) {
-       let apiRequestBillRest = "https://congress.api.sunlightfoundation.com/bills?bill_id=" + value.bill_id;
-       console.log(apiRequestBillRest);
-       request(apiRequestBillRest, function(error, response, bodyBillsRest) {
-         // Print error if occured
-         console.log("error: ", error);
-
-         // Print response status code if recieved
-         console.log("statusCode: ", response && response.statusCode);
-
-         // Make final bill object and push it into complete array bills array
-         if(!error){
-           let billComplete = billStatsComplete(value, JSON.parse(bodyBillsRest).results[0]);
-           recBillsArrayComplete.push(billComplete);
-         }
-       });
-     });
-   });
- }
+      let completeBill = billStatsComplete(billPartial, JSON.parse(bodyBillsRest).results[0]);
+      //console.log(completeBill);
+      resolve(completeBill);
+    });
+  })
+}
 
 function billStatsComplete(billPartial, bodyBillsRest) {
   let billComplete = new BillComplete(
@@ -175,66 +176,29 @@ function BillComplete(bill_id, vote, official_title, url) {
   this.url = url;
 }
 
-// function for find out info with body
-function billStatsPartial(bodyBillsPartial, recBillsArrayPartial, bioguide_id) {
 
-  // set BillInfo to the meat of our api (contains bill_info and vote)
-  let billInfo = bodyBillsPartial.results;
-
-  // Add all bills and corresponding votes to recBillsArrayPartial
-  for(let i = 0; i < billInfo.length; i++) {
-    if(billInfo[i].bill_id !== undefined){
-      let billPartial = new BillPartial(
-        billInfo[i].bill_id,
-        billInfo[i].voters[bioguide_id].vote
-      );
-      // add the new billPartial to our array
-      recBillsArrayPartial.push(billPartial);
-    }
-  }
-};
-
-
-// Constructor for billPartial (bill_id and vote in regars to bill_id)
-function BillPartial(bill_id, vote) {
-  this.bill_id = bill_id;
-  this.vote = vote;
-}
-
+////////////////////  TESTS ///////////////////////////
+// TEST 1: Retrieve legislator info and display
+// WORKS!!!!
 /*
-// Constructor for legislator (phone_number, website)
-function Bill(bill_id, vote, url, official_title){
-  this.bill_id = bill_id;
-  this.vote = vote;
-  this.url = url;
-  this.official_title = official_title;
-}
+let testZip = legislatorInfoByZipCode("65201")
+.then((result) => {
+  console.log(result);
+});
+*/
 
-// function for find out info with body
-function billStats(bodyBills, recBillsArray){
-  _.each(bodyBills.results, function(value, key){
-    if(value.bill_id !== undefined){
-      // There's a bill id so we need to query the rest of the bill info
-      let restInfo = "https://congress.api.sunlightfoundation.com/bills?bill_id=" + value.bill_id;
-
-      // We make one more request for rest of info (official_title, urls.govtrack)
-      request(restInfo, function(error, response, body) {
-        console.log("error: ", error);
-        console.log("statusCode: ", response && response.statusCode);
-
-        let billObject = JSON.parse(body);
-
-        let bill = new Bill(
-          value.bill_id,
-          value.vote,
-          billObject.urls.govtrack,
-          billObject.official_title
-        );
-        recBillsArray.push(legislator);
-      });
+// Test 2: Retrieve bill info and display
+// WORKS!!!
+/*
+let testLegislator = billInfoRequest("H001053")
+.then(
+  (result) => Promise.all(result.map(
+    (value) => {
+      return getCompleteBill(value);
     }
-  });
-}
-
-
+  ))
+)
+.then((result) => {
+  console.log(result);
+});
 */
