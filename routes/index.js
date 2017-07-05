0ab06e22431c75f22bc302deabd36a3a2db129df
Will Timpson <will.timpson@gmail.com>
2017-07-05 16:31:56 -0400

    representatives

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const title = 'What Have You Done?';
+const sunlight = require('../lib/sunlight');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+
+  if (req.query.zip) {
+    sunlight.getLegislators(req.query.zip, function(err, legislators) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('index', {
+          title: title,
+          zip: req.query.zip,
+          legislators: legislators
+        });
+      }
+    })
+  } else {
+    res.render('index', { title: title, zip: req.query.zip});
+  }
 });
 
 module.exports = router;
