cd16563c7eded6283905a4c86e0b5b502be45c47
Will Timpson <will.timpson@gmail.com>
2017-07-04 21:15:59 -0400

    Boilerplate

---
+++ sunlight.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const rp = require('request-promise-native')
+
+const baseUrl = 'https://congress.api.sunlightfoundation.com/'
+
+const sunlight = {
+
+  getLegislators: function(zipCode) {
+
+    // return the legislators for a given zip code 
+
+  },
+
+  getVotes: function(legislator) {
+
+    // return the votes of a given legislator
+
+  }
+
+}
+
+module.exports = sunlight;
