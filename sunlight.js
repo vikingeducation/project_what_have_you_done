5069095002f33fe02c77b3801eb5236b34f147dc
Will Timpson <will.timpson@gmail.com>
2017-07-05 10:24:22 -0400

    get votes!

--- sunlight.js
+++ sunlight.js
@@ -11,17 +11,11 @@ const sunlight = {
 
     // return the legislators for a given zip code 
 
-    const url = baseUrl + 'legislators/locate?zip=' + zipCode;
-    const options = {
-      uri: url,
-      json: true
-    };
-    rp(options)
-      .then(function(response) {
-        const fields = [
+    const fields = [
           'bioguide_id',
           'chamber',
           'facebook_id',
+          'nickname',
           'first_name',
           'middle_name',
           'last_name',
@@ -34,13 +28,14 @@ const sunlight = {
           'youtube_id'
         ]
 
-        let parsedPersons = []
-
-        _.each(response.results, function(legislator) {
-          parsedPersons.push(_.pick(legislator, fields));
-        })
-
-        console.log(parsedPersons);
+    const url = baseUrl + 'legislators/locate?zip=' + zipCode + '&fields=' + fields.join(',');
+    const options = {
+      uri: url,
+      json: true
+    };
+    rp(options)
+      .then(function(response) {
+       console.log(response.results);
       })
       .catch(function(error) {
         console.log(error);
@@ -51,6 +46,34 @@ const sunlight = {
   getVotes: function(legislator) {
 
     // return the votes of a given legislator
+
+    const url = baseUrl + 'votes?order=voted_at&fields=voters,roll_id,question,voted_at&per_page=50';
+    const options = {
+      uri: url,
+      json: true
+    }
+    rp(options)
+      .then(function(response) {
+        let billz = [];
+
+        _.each(response.results, function(bill) {
+          if (bill.voters[legislator]) {
+            let currentBill = {
+              'title': bill.question,
+              'id': bill.roll_id,
+              'time': bill.voted_at,
+              'vote': bill.voters[legislator].vote
+            }
+            billz.push(currentBill);
+          }
+        })
+
+        console.log(billz);
+
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
 
   }
 
