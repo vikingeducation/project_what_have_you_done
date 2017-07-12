ab25781f1ec1179b868f3c4b517a8be03ff2fe7a
Stephanie Barker <song_mage@yahoo.com>
2017-07-12 16:17:01 -0400

    generated bill links; cleaned up styles

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
 			alert("Please enter a 5-digit zip code.");
 		}
 	});
+
+	$(".vote").each(function() {
+		var bill = $(this).find("h2").text();
+
+		console.log(bill);
+
+		var billParts = bill.split("-");
+
+		var url = `https://www.govtrack.us/congress/bills/${billParts[1]}/${billParts[0]}`;
+
+		var link = $(this).parent();
+
+		link.attr("href", url);
+		link.addClass("no-style");
+	});
 });
