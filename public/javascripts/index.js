eb4bbc6b28ad4478cae380eb3d28dda1220bc88d
Ian Halverson <halversoncode@gmail.com>
2017-07-12 19:54:07 -0400

    finished

---
+++ public/javascripts/index.js
@@ -0,0 +1,96 @@
+
+var states = {
+  	AL:1,MT:1,AK:1,NE:1,AZ:1,NV:1,AR:1,NH:1,CA:1,NJ:1,CO:1,NM:1,CT:1,NY:1,DE:1,
+    NC:1,FL:1,ND:1,GA:1,OH:1,HI:1,OK:1,ID:1,OR:1,IL:1,PA:1,IN:1,RI:1,IA:1,SC:1,
+    KS:1,SD:1,KY:1,TN:1,LA:1,TX:1,ME:1,UT:1,MD:1,VT:1,MA:1,VA:1,MI:1,WA:1,MN:1,
+    WV:1,MS:1,WI:1,MO:1,WY:1
+}
+
+var f = {
+  headerRedirect: function(){
+    $("h1").click(function(){
+      window.location.href = "http://localhost:3000";
+    })
+  },
+  
+  typeLetter: function(){
+    $(document).on("keypress", function(event){
+      if ((/^[a-zA-Z]*$/.test(event.key)) && event.which!==13) {
+        if ($(".letter").first().hasClass("active-div")) {
+          $(".letter-text.active").html(event.key.toUpperCase());
+          $(".letter").toggleClass("active-div");
+          $(".letter-text").toggleClass("active");
+        }
+        else {
+          $(".letter-text.active").html(event.key.toUpperCase());
+          $(".letter.active-div").toggleClass("active-div");
+          $(".letter-text.active").toggleClass("active");
+          $(".submit").slideDown(300);
+        }
+      }
+    })
+  },
+
+  deleteLetter: function(){
+    $(document).keydown(function(){
+      if (event.which===8){
+        if ($(".letter.active-div").length===0){
+          $(".submit").slideUp(300);
+          $(".letter").last().toggleClass("active-div");
+          $(".letter-text").last().toggleClass("active").html("-");
+        }
+        else {
+          if ($(".letter").last().hasClass("active-div")) {
+            $(".letter").toggleClass("active-div");
+            $(".letter-text").toggleClass("active");
+            $(".letter-text").first().html("-");
+          }
+        }
+      }
+    })
+  },
+
+  submitState: function(){
+    $(".submit").click(function(event){
+      var stateL1 = $(".letter-text").first().html();
+      var stateL2 = $(".letter-text").last().html();
+      var state = stateL1 + stateL2;
+      if (!(state in states)) {
+        alert("Please enter a valid state.");
+      }
+      else {
+        var url = "http://localhost:3000/states/" + state;
+        window.location.href = url;
+      }
+   })
+ },
+
+ enterPress: function(){
+   $(document).keydown(function(event){
+     if (event.which===13){
+       if ($(".letter.active-div").length===0){
+         var stateL1 = $(".letter-text").first().html();
+         var stateL2 = $(".letter-text").last().html();
+         var state = stateL1 + stateL2;
+         if (!(state in states)) {
+           alert("Please enter a valid state.");
+         }
+         else {
+           var url = "http://localhost:3000/states/" + state;
+           window.location.href = url;
+         }
+       }
+     }
+   })
+ }
+
+}
+
+$(document).ready(function(){
+  f.headerRedirect();
+  f.typeLetter();
+  f.deleteLetter();
+  f.submitState();
+  f.enterPress();
+
+})
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const fs = require('fs');
-app.set('view engine', 'hbs');
+const path = require('path');
 
-app.listen(process.env.PORT, function(){
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+
+app.listen(3000, function(){
   console.log("listening at port 3000");
 })
 
@@ -14,11 +18,46 @@ app.get("/", function(req, res){
 
 var jsonData;
 
+
+
 app.get("/states/:state", function(req, res) {
+  var stateMetadata;
+  var stateBills;
+  var prom1;
+  var prom2;
   var state = req.params.state;
-  var url = "https://openstates.org/api/v1/metadata/"+state;
-  request({uri: url, headers: {"X-API-KEY": "59bcba1f-e98d-45b7-a2ef-8658a3e4a56f"}}, function(error, response, body){
-  jsonData = JSON.parse(body);
+  var prom1 = new Promise(function(resolve, reject){
+    var url = "https://openstates.org/api/v1/metadata/"+state;
+    request({uri: url, headers: {"X-API-KEY": "59bcba1f-e98d-45b7-a2ef-8658a3e4a56f"}}, function(error, response, body){
+    stateMetadata = JSON.parse(body);
+    resolve(body);
+    })
   })
-  res.render('mystate.hbs', {state: jsonData});
+  var prom2 = new Promise(function(resolve, reject){
+    var url2 = "https://openstates.org/api/v1/bills/?state="+state+"&q="+state;
+    request({uri: url2, headers: {"X-API-KEY": "59bcba1f-e98d-45b7-a2ef-8658a3e4a56f"}}, function(error, response, body){
+    stateBills = JSON.parse(body);
+    resolve(body);
+    })
+  })
+  var prom3 = new Promise(function(resolve, reject){
+    var url2 = "https://openstates.org/api/v1/legislators/?state="+state;
+    request({uri: url2, headers: {"X-API-KEY": "59bcba1f-e98d-45b7-a2ef-8658a3e4a56f"}}, function(error, response, body){
+    stateLegislators = JSON.parse(body);
+    resolve(body);
+    })
+  })
+  var prom4 = new Promise(function(resolve, reject){
+    var url2 = "https://openstates.org/api/v1/committees/?state="+state;
+    request({uri: url2, headers: {"X-API-KEY": "59bcba1f-e98d-45b7-a2ef-8658a3e4a56f"}}, function(error, response, body){
+    stateCommittees = JSON.parse(body);
+    resolve(body);
+    })
+  })
+  Promise.all([prom1, prom2, prom3, prom4]).then(function(results){
+    res.render('mystate.hbs', {meta: stateMetadata, bills: stateBills, legislators: stateLegislators, committees: stateCommittees});
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 })
