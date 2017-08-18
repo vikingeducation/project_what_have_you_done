71b4d87f93ee7324e6290d0193fd7ab6617d57f0
anne <richardson.ae@gmail.com>
2017-08-18 15:01:34 -0500

    WIP feeling the struggle of not passing form params into the router correctly

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var request = require('request');
 var index = require('./routes/index');
 var zip_results_router = require('./routes/zip_results_router');
 var legislator_router = require('./routes/legislator_router');
-var users = require('./routes/users');
 
 // express app
 var app = express();
@@ -27,7 +26,7 @@ app.set('view engine', 'hbs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/zip_results', zip_results_router);
 app.use('/legislators', legislator_router);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/zip_results_router.js
+++ routes/zip_results_router.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+// var {SunlightApi} = require('../api/sunlight_api');
 const {Legislator, LEGISLATORS} = require('../models/legislators_model')
 
-router.get('/', function(req, res, next) {
-  res.render('zip_results', { title: 'Legislators by Zip' });
-});
-
 router.get('/:zip', function(req, res, next) {
-  res.render('zip_results_view', { title: 'Legislators by zip', legislators: LEGISLATORS });
+  var zip_from_params = req.params.zip;
+  // var api = new SunlightApi(zip_from_params);
+  // api.get_legislators();
+  // res.send('you sent ' + zip_from_params);
+  res.render('zip_results_view', { title: 'Legislators by zip', legislators: LEGISLATORS, from_params: zip_from_params });
 });
 
 module.exports = router
