f0bd76fcd16c9316bacc9746a7f53bf97b1270e8
anne <richardson.ae@gmail.com>
2017-08-23 09:48:20 -0500

    Separate members of house and senate on legislators index page

--- routes/legislators_index_router.js
+++ routes/legislators_index_router.js
@@ -2,15 +2,22 @@ var express = require('express');
 var router = express.Router();
 var {SunlightApi} = require('../api/sunlight_api');
 
+var getMembers = function(legislators, position) {
+  return members = legislators.filter(function(legislator){ return legislator.chamber === position});
+};
+
 router.get('/', function(req, res, next) {
-  // res.send('you sent ' + req.query.zip);
   var zip = req.query.zip;
   var api = new SunlightApi();
 
-  // make a call to the api to get legislators for this zip
+  // make a call to the api to get legislators for this zip.
+  // this is the action of the callback referenced api function
   api.getLegislators(zip, function(legislators){
-    // this is the action of the callback referenced api function
-    res.render('legislators_index', { title: 'Legislators by zip', legislators: legislators, zip_numbers: zip.split('') });
+    // separate results by house/senate
+    var houseMembers = getMembers(legislators, 'house');
+    var senateMembers = getMembers(legislators, 'senate');
+    // render this info in the view
+    res.render('legislators_index', { title: 'Legislators by zip', house_members: houseMembers, senate_members: senateMembers, zip_numbers: zip.split('') });
   });
 });
 
