2a346bc5a5db3f1fe7184a4400c3b59a546b13a5
anne <richardson.ae@gmail.com>
2017-08-23 14:12:33 -0500

    Standardize output vote results

--- models/vote_model.js
+++ models/vote_model.js
@@ -2,14 +2,34 @@ class Vote {
   // Take the full results object and parse it into useful fields for the views
   constructor(params, bioguide_id) {
     this.billId = params.bill_id,
-    this.result = params.result,
+    this.result = this.resultConverter(params.result),
     this.legislatorId = bioguide_id,
     this.legislatorVote = this.voteParser(params.voter_ids, bioguide_id)
   };
 
   voteParser(voterIds, bioguide_id){
-    return voterIds[bioguide_id];
-  }
+    switch (voterIds[bioguide_id]) {
+      case 'Yea':
+        return 'Yes'
+        break;
+      case 'Nay':
+          return 'No'
+          break;
+      default:
+       return voterIds[bioguide_id];
+    }
+  };
+
+  resultConverter(apiResult){
+    apiResult = apiResult.toLowerCase();
+    if (apiResult.includes('pass') || apiResult.includes('agree')) {
+      return 'Passed'
+    } else if (apiResult.includes('fail')) {
+      return 'Failed'
+    } else {
+      return apiResult
+    }
+  };
 
 } // close Vote
 
