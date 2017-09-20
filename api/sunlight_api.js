829ea7f8143e97bc119f4e199cfd46823e2dd4a9
maddiereddy <maddiereddy@gmail.com>
2017-09-19 21:07:14 -0700

    modified the votes url

--- api/sunlight_api.js
+++ api/sunlight_api.js
@@ -23,7 +23,7 @@ class SunlightApi {
 
  
   getLegislatorVotes(bioguide_id, callback) {
-    var url = `https://congress.api.sunlightfoundation.com/votes?fields=bill,voter_ids&vote_type=passage&order=voted_at&voter_ids.${bioguide_id}__exists=true`;
+    var url = `${baseUri}/votes?fields=bill,voter_ids&vote_type=passage&order=voted_at&voter_ids.${bioguide_id}__exists=true`;
     var data = (result) => new Bill(result, bioguide_id);
 
     this._sendRequest(url, data, callback);
