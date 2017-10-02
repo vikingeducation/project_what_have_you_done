5881ae5404669c72b0c65b7d6dc08278ae3fe019
Jared Gebel <jaredjgebel@gmail.com>
2017-10-02 16:49:51 -0400

    Initialize express application structure

---
+++ index.js
@@ -0,0 +1,7 @@
+// Define API URL components
+// Google Civics API
+var apiGC = "https://www.googleapis.com/civicinfo/v2/representatives?address=";
+// Encode
+var userAddress = input;
+
+var url = apiGC + userAddress;
