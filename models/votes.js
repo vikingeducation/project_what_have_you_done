3320f83e4a0f541e789c9ca25c59943cf7615682
Jared Gebel <jaredjgebel@gmail.com>
2017-11-01 16:57:45 -0400

    Add votes class

---
+++ models/votes.js
@@ -0,0 +1,21 @@
+class Vote {
+  constructor(obj, i) {
+    this.billNumber = obj.results[0].votes[i].bill.number;
+    this.description = obj.results[0].votes[i].description;
+    this.question = obj.results[0].votes[i].question;
+    this.result = obj.results[0].votes[i].result;
+    this.date = obj.results[0].votes[i].date;
+    this.time = obj.results[0].votes[i].time;
+    this.totalYes = obj.results[0].votes[i].total.yes;
+    this.totalNo = obj.results[0].votes[i].total.no;
+    this.notVoting = obj.results[0].votes[i].total.not_voting;
+    this.position = obj.results[0].votes[i].position;
+  }
+}
+
+module.exports = {
+  Vote
+}
+
+
+// Pass in constructor: obj
